c161e68f48c721e9340b2924c83ffbc5716ed9a3
durga prasad <nprasad@nprasad-lp.pkrmsezdc.com>
2021-06-21 23:03:57 +0530

    salesforce added

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ const express = require("express");
 const app = express();
 const { Client } = require('pg')
 const client = new Client({
-    host: 'ec2-107-21-10-179.compute-1.amazonaws.com',
-    port: 5432,
-    user: 'xducephjlnjuon',
-    password: '8da6c1fb84f2d71f541f4fc80c5409bc97503742d631df371b52761422b5efa6',
-    connectionString: 'postgres://xducephjlnjuon:8da6c1fb84f2d71f541f4fc80c5409bc97503742d631df371b52761422b5efa6@ec2-107-21-10-179.compute-1.amazonaws.com:5432/d5v88rtqervvkb'
+    connectionString: "postgres://xducephjlnjuon:8da6c1fb84f2d71f541f4fc80c5409bc97503742d631df371b52761422b5efa6@ec2-107-21-10-179.compute-1.amazonaws.com:5432/d5v88rtqervvkb",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 client.connect();
 
+app.set('view engine', 'ejs');
+
 app.get("*", (req, res) => {
     // res.json({ text: "Hello salesforce" })
+
     client.query('SELECT * FROM salesforce.contact', (err, result) => {
         //console.log(err ? err.stack : res.rows[0].message) // Hello World!
-        console.log(err, result);
-        client.end()
-        res.json(result);
+        // client.end()
+        //res.json(result.rows);
+        res.render('index', {
+            contacts: result.rows
+        });
     })
 })
 
