c5878db77907c8b68854963b0ef8127a5bc1e8ae
durga prasad <nprasad@nprasad-lp.pkrmsezdc.com>
2021-06-21 19:30:53 +0530

    salesforce added

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const pg = require('pg');
 
 app.get("*", (req, res) => {
     res.json({ text: "Hello salesforce" })
+
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'SELECT * FROM salesforce.contact',
+            function (err, result) {
+                done();
+                res.json(result);
+            }
+        );
+    });
 })
 
 app.listen(process.env.PORT || 4444, () => {
