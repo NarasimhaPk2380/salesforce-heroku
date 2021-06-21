ce4156fc46d142901b56a322533ba9615016d5a1
durga prasad <nprasad@nprasad-lp.pkrmsezdc.com>
2021-06-21 20:00:22 +0530

    salesforce added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const client = new Client({
     port: 5432,
     user: 'xducephjlnjuon',
     password: '8da6c1fb84f2d71f541f4fc80c5409bc97503742d631df371b52761422b5efa6',
-    ssl: true
+    connectionString: 'postgres://xducephjlnjuon:8da6c1fb84f2d71f541f4fc80c5409bc97503742d631df371b52761422b5efa6@ec2-107-21-10-179.compute-1.amazonaws.com:5432/d5v88rtqervvkb'
 })
 client.connect();
 
